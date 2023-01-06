4bf232e39f8ff52a87cb470e7988e340c6118d72
Alina <alinascarlett@mail.ru>
2023-01-06 21:49:50 +0300

    slider

--- source/js/modules/init-cats-slider.js
+++ source/js/modules/init-cats-slider.js
@@ -1,14 +1,31 @@
 const initCatsSlider = () => {
-  const swiper = new Swiper('.cats__swiper-container',
-      {
-        slidesPerView: 1,
-        loop: true,
-        spaceBetween: 10,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+
+  const catsSlider = document.querySelector('.cats__swiper-container');
+  // const desktop = window.matchMedia('(min-width: 1025px)');
+
+  const initSwiper = (slider) => {
+  // eslint-disable-next-line no-undef, no-unused-vars
+    let swiper = new Swiper(slider, {
+      speed: 600,
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 23,
+      watchOverflow: true,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 10,
         },
-      });
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+      },
+    });
+  };
+
+  initSwiper(catsSlider);
+
 };
 
 export {initCatsSlider};
